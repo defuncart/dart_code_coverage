83625578dfd036fbfbd3196550f5283cfb28e2d3
James Leahy <defuncart@gmail.com>
2020-10-16 13:05:41 +0200

    Feature/add ci pull request workflow (#3)
    Adds a ci workflow for pull requests to run code analysis and unit tests.
    
    Commits:
    * Add fake test.
    * Add pedantic dependency, analysis_options.yaml.
    * Fix analyzer warnings.
    * Add workflow.

--- bin/utils/reg_exp_utils.dart
+++ bin/utils/reg_exp_utils.dart
@@ -2,15 +2,15 @@
 abstract class RegExpUtils {
   /// Combines a list of string patterns into a RegExp
   static RegExp combinePatterns(List<String> patterns) {
-    if (patterns != null && patterns.length > 0) {
-      String combinedPattern = '';
-      for (int i = 0; i < patterns.length; i++) {
+    if (patterns != null && patterns.isNotEmpty) {
+      var combinedPattern = '';
+      for (var i = 0; i < patterns.length; i++) {
         if (patterns[i] == null || patterns[i].isEmpty) {
           continue;
         }
 
         if (i != 0) {
-          combinedPattern += "|";
+          combinedPattern += '|';
         }
         combinedPattern += patterns[i];
       }
